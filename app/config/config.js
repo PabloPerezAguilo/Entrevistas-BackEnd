894dd6a4f44f6c048a68dcc3482266df53b15180
enriquefromojaro <enriquefromojaro@gmail.com>
2015-11-02 13:26:30 +0100

    Unión de la parte de control de acceso por roles realizada con el nuevo modelo de usuario

--- app/config/config.js
+++ app/config/config.js
@@ -3,5 +3,7 @@ var uuidgen=uuid.v4();
 module.exports = {
 	'secret': uuidgen,
 	'database': 'mongodb://localhost/MagniDB',
-	'cacheEnabled':true
+	'cacheEnabled':true,
+    'ROLE_ADMIN':'ROLE-ADMIN',
+    'ROLE_TECH': 'ROLE_TECH'
 };
---
+++ app/controllers/interviewController.js
@@ -0,0 +1,5 @@
+// Load required packages
+var User = require('../models/user');
+var log4js = require('log4js');
+var log = log4js.getLogger("interviewCtrl");
+
