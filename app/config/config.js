d7f2aca5571134940959a67d5d40c1946134f7de
enriquefromojaro <enriquefromojaro@gmail.com>
2015-11-02 11:41:16 +0100

    Ahora la base datos es MagniDB.

--- app/config/config.js
+++ app/config/config.js
@@ -2,6 +2,6 @@ var uuid = require('node-uuid');
 var uuidgen=uuid.v4();
 module.exports = {
 	'secret': uuidgen,
-	'database': 'mongodb://localhost/apinode',
+	'database': 'mongodb://localhost/MagniDB',
 	'cacheEnabled':true
 };
